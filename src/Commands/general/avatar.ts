52da819e679f5fa3ecbc79728c22c176b15757a2
lyem <rfael403@gmail.com>
2022-05-07 14:42:56 -0300

    created command avatar

---
+++ src/Commands/general/avatar.ts
@@ -0,0 +1,27 @@
+import { Constants, MessageEmbed } from 'discord.js'
+import { Command } from '../../Interfaces'
+
+export const slash: Command = {
+  name: 'avatar',
+  description: 'Show avatar user',
+  options: [
+    {
+      name: 'user',
+      description: 'select user',
+      type: Constants.ApplicationCommandOptionTypes.USER,
+      required: true
+    }
+  ],
+  testOnly: false,
+  run: async ({ interaction }) => {
+    await interaction.deferReply()
+    const user = interaction.options.getUser('user')
+    const url = user?.avatarURL({ format: 'jpg' })
+    const embed = new MessageEmbed()
+    embed.setColor('#206694')
+    embed.setImage(url!)
+    embed.setTitle('Baixar')
+    embed.setURL(url!)
+    return await interaction.followUp({ embeds: [embed] })
+  }
+}
