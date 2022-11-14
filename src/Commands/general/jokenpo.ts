1da93b1a99f1777ee103c077daabfb34ecf06c31
Gabriel Alves <112426768+gaba23@users.noreply.github.com>
2022-11-14 19:28:58 -0300

    Update jokenpo.ts

--- src/Commands/general/jokenpo.ts
+++ src/Commands/general/jokenpo.ts
@@ -2,7 +2,7 @@ import { Constants, MessageEmbed } from 'discord.js'
 import { Command } from '../../Interfaces'
 
 export const slash: Command = {
-  name: 'Pedra, papel ou tesoura',
+  name: 'Jokenpo',
   description: 'Que vença o mais ~~sortudo~~ melhor... cof cof',
   options: [
     {
