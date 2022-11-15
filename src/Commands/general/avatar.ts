598b73fe02378c7e5db2da94e14126e0b20a461f
Rafael Corrêa de Melo <36901020+Lyem@users.noreply.github.com>
2022-11-14 21:07:07 -0300

    Fix avatar return
    return dynamic formats

--- src/Commands/general/avatar.ts
+++ src/Commands/general/avatar.ts
@@ -16,7 +16,7 @@ export const slash: Command = {
   run: async ({ interaction }) => {
     await interaction.deferReply()
     const user = interaction.options.getUser('user')
-    const url = user?.avatarURL({ format: 'jpg' })
+    const url = user?.avatarURL({ size: 4096, dynamic: true })
     const embed = new MessageEmbed()
     embed.setColor('#206694')
     embed.setImage(url!)
