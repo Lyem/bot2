ab6e11ae2ec3daa30d3a66c1846f7e509a9ae78d
lyem <rfael403@gmail.com>
2022-12-25 16:37:34 -0300

    add new command

---
+++ src/Commands/general/countServers.ts
@@ -0,0 +1,14 @@
+import { Command } from '../../Interfaces'
+
+export const slash: Command = {
+  name: 'count_servers',
+  description: 'returns how many servers this bot is on',
+  testOnly: false,
+  run: async ({ interaction }) => {
+    await interaction.deferReply()
+    const guilds = await interaction.client.guilds.cache.size
+    return interaction.followUp(
+      `este bot está em: **${guilds} ${guilds > 1 ? 'servers' : 'server'}**`
+    )
+  }
+}
